aedf24fe4a43ef044fa3fa208264e1997149f545
IanLin012 <95599572+IanLin012@users.noreply.github.com>
2022-01-08 23:28:56 +0800

    Update FacePI.py

--- FacePI.py
+++ FacePI.py
@@ -5,13 +5,10 @@ import classes.ClassFaceAPI
 import classes.ClassPerson
 import classes.ClassPersonGroup
 import classes.ClassConfig
-
 config = classes.ClassConfig.Config().readConfig()
-
 class FacePI:
     def show_opencv(self):
         classes.ClassOpenCV.show_opencv("")
-
     def Identify(self, pictureurl):
         """14: 進行「辨識」，使用 image URL or 檔案路徑"""
         start = int(round(time.time() * 1000))
@@ -19,47 +16,41 @@ class FacePI:
         faceApi = classes.ClassFaceAPI.Face()
         personApi = classes.ClassPerson.Person()
         print("載入 class", int(round(time.time() * 1000) - start), "ms")
-        # imageurl = input('請輸入準備要辨識的 image URL or 檔案路徑:')
+        imageurl = input('請輸入準備要辨識的 image URL or 檔案路徑:')
         if pictureurl.startswith("http"):
             detectfaces = faceApi.detectURLImages(pictureurl)
         else:
             pictureurl = pictureurl.strip()
-            # statinfo = os.stat(pictureurl)
-            # print("檔案大小：", statinfo.st_size, "Bytes")
-            # if statinfo.st_size < 1024:
-            #     print("圖檔太小 不可小於 1KB")
-            #     sys.exit(1)
-            # elif statinfo.st_size > 4 * 1024 * 1024:
-            #     print("圖檔太大 不可大於 4MB")
-            #     im = Image.open(pictureurl)
-            #     out = im.resize((128, 128))
-            #     im.save(pictureurl, "JPEG")
-            #     print("out=", type(out))
+            statinfo = os.stat(pictureurl)
+            print("檔案大小：", statinfo.st_size, "Bytes")
+            if statinfo.st_size < 1024:
+                print("圖檔太小 不可小於 1KB")
+                sys.exit(1)
+            elif statinfo.st_size > 4 * 1024 * 1024:
+                print("圖檔太大 不可大於 4MB")
+                im = Image.open(pictureurl)
+                out = im.resize((128, 128))
+                im.save(pictureurl, "JPEG")
+                print("out=", type(out))
             detectfaces = faceApi.detectLocalImage(pictureurl)
-
-        # if len(detectfaces) == 0:
-        #     print('相片中找不到人！')
-        #     sys.exit(1)
-
+        if len(detectfaces) == 0:
+            print('相片中找不到人！')
+            sys.exit(1)
         faceids = []
         for detectface in detectfaces:
             print("所偵測到的 faceId=", detectface["faceId"])
             faceids.append(detectface["faceId"])
-
         print("Identify.detectfaces=", detectfaces)
-
         identifiedfaces = faceApi.identify(faceids[:10], config["personGroupId"])
-        # print("在所提供的相片中偵測到 identifyfaces 共 ", len(identifiedfaces), "個")
-        # except MyException.PersonGroupNotTrainedError as e:
-        #     print("接到例外！MyException.PersonGroupNotTrainedError as e")
-        #     print("Identify.detectedFaces=", detectfaces)
-        #     ClassCV.cv_Identifyfaces(detectfaces, pictureurl)
-        #     # ClassTK.tk_UnknownPerson('texttest....', pictureurl, pictureurl)
-
-        #     return
         print("在所提供的相片中偵測到 identifyfaces 共 ", len(identifiedfaces), "個")
-
-        # successes = []
+        except MyException.PersonGroupNotTrainedError as e:
+            print("接到例外！MyException.PersonGroupNotTrainedError as e")
+            print("Identify.detectedFaces=", detectfaces)
+            ClassCV.cv_Identifyfaces(detectfaces, pictureurl)
+            ClassTK.tk_UnknownPerson('texttest....', pictureurl, pictureurl)
+            return
+        print("在所提供的相片中偵測到 identifyfaces 共 ", len(identifiedfaces), "個")
+        successes = []
         for identifiedface in identifiedfaces:
             for candidate in identifiedface["candidates"]:
                 personId = candidate["personId"]
@@ -67,8 +58,7 @@ class FacePI:
                 identifiedface["person"] = person
                 identifiedface["confidence"] = candidate["confidence"]
                 identifiedface["personId"] = candidate["personId"]
-
-        # cv_Identifyfaces() 精簡版
+        #cv_Identifyfaces() 精簡版
         for identifyface in identifiedfaces:
             if "person" not in identifyface:
                 print("identifyface=", identifyface)
@@ -84,21 +74,12 @@ class FacePI:
                     print(name + " 簽到成功!")
                 else:
                     print(name + " 簽到成功")
-
     def Signin(self):
         """
         刷臉簽到
         """
-        # imagepath = '202994853.jpg'
-        # imagepath = 'face4.jpg'
-        # self.detectLocalImage(imagepath)
-        # imageurl = 'https://cdn-news.readmoo.com/wp-content/uploads/2016/07/Albert_einstein_by_zuzahin-d5pcbug-1140x600.jpg'
-        # imageurl = 'https://cdn2.momjunction.com/wp-content/uploads/2020/11/facts-about-albert-einstein-for-kids-720x810.jpg'
-        # classes.ClassFaceAPI.Face().detectImageUrl(imageurl)
         imagepath = classes.ClassOpenCV.show_opencv()
-        # json_face_detect = classes.ClassFaceAPI.Face().detectLocalImage(imagepath)
         self.Identify(imagepath)
-
     def Train(self, userData=None, personname=None):
         """1. 用 3 連拍訓練一個新人"""
         jpgimagepaths = []
@@ -107,18 +88,15 @@ class FacePI:
                 hint=" (訓練第 " + str(i + 1) + " 張)"
             )
             jpgimagepaths.append(jpgimagepath)
-
         if personname == None:
             personname = input("請輸入您的姓名: ")
-
         if userData == None:
             userData = input("請輸入您的說明文字(比如: 高師大附中國一仁): ")
-
         basepath = os.path.dirname(os.path.realpath(__file__))
         jpgtrainpaths = []
         for jpgimagepath in jpgimagepaths:
             filename = os.path.basename(jpgimagepath)
-            # home = os.path.expanduser("~")
+            home = os.path.expanduser("~")
             jpgtrainpath = os.path.join(
                 basepath, "traindatas", userData, personname, filename
             )
@@ -126,16 +104,12 @@ class FacePI:
                 os.makedirs(os.path.dirname(jpgtrainpath))
             os.rename(jpgimagepath, jpgtrainpath)
             jpgtrainpaths.append(jpgtrainpath)
-
         myconfig = classes.ClassConfig.Config().readConfig()
-
         personAPI = classes.ClassPerson.Person()
         personAPI.add_personimages(
             myconfig["personGroupId"], personname, userData, jpgtrainpaths
         )
         personGroupapi = classes.ClassPersonGroup.PersonGroup()
         personGroupapi.train_personGroup()
-
-
 if __name__ == "__main__":
     fire.Fire(FacePI)
